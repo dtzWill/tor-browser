746ea2f36d64ce034cbb83ccfa2546872cb2ce66
SLNOS <anonymous@wired>
2017-04-01 00:00:00 +0000

    Revert "Bug 13252 - Do not store data in the app bundle"
    This reverts commit ea93541574e90e0b62ffc3cd3b18c169975e75c2.

--- toolkit/xre/nsXREDirProvider.h
+++ toolkit/xre/nsXREDirProvider.h
@@ -100,12 +100,6 @@ public:
    */
   nsresult GetProfileDir(nsIFile* *aResult);
 
-  /**
-   * Get the TorBrowser user data directory by calling the
-   * TorBrowser_GetUserDataDir() utility function.
-   */
-  nsresult GetTorBrowserUserDataDir(nsIFile* *aFile);
-
 protected:
   nsresult GetFilesInternal(const char* aProperty, nsISimpleEnumerator** aResult);
   nsresult GetUserDataDirectoryHome(nsIFile* *aFile, bool aLocal);
@@ -113,6 +107,7 @@ protected:
 #if defined(XP_UNIX) || defined(XP_MACOSX)
   static nsresult GetSystemExtensionsDirectory(nsIFile** aFile);
 #endif
+  nsresult GetAppRootDir(nsIFile* *aFile);
   static nsresult EnsureDirectoryExists(nsIFile* aDirectory);
   void EnsureProfileFileExists(nsIFile* aFile);
 
